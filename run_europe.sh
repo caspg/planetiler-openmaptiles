030f34ef2d8f1d1f9cbc9eefdd3231d24905b390
Kacper Golinski <kacper.golinski@gmail.com>
2024-04-02 09:19:25 +0200

    Update run_europe.sh

--- run_europe.sh
+++ run_europe.sh
@@ -9,7 +9,6 @@ JAVA_TOOL_OPTIONS="-Xmx32g" java -jar target/*with-deps.jar \
   --bounds=planet \
   --max_point_buffer=4 \
   --exclude_layers=housenumber \
-  --download-threads=10 --download-chunk-size-mb=1000 \
+  --download-threads=5 --download-chunk-size-mb=1000 \
+  --threads=5
   --nodemap-type=array --storage=mmap 2>&1 | tee logs.txt
-  # --download-threads=5 --download-chunk-size-mb=1000 \
-  # --threads=5
