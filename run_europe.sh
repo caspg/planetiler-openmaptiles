2a2983126ad48558dbbedb432636d13757872597
Kacper Golinski <kacper.golinski@gmail.com>
2024-04-23 13:52:10 +0200

    Update run_europe.sh

--- run_europe.sh
+++ run_europe.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# - MaxHeapFreeRatio - return unused heap memory to the OS
-
-# - --storage=ram - use RAM storage for temporary data
-# which requires 1.5x more memory than the input data size
-# Europe OSM data is about 30GB so we need at least 45GB of RAM
-
-JAVA_TOOL_OPTIONS="-Xmx48g" java -jar target/*with-deps.jar \
-  -XX:MaxHeapFreeRatio=40 \
+JAVA_TOOL_OPTIONS="-Xmx32g" java -jar target/*with-deps.jar \
   --force \
   --download \
   --area=europe  \
@@ -16,7 +9,8 @@ JAVA_TOOL_OPTIONS="-Xmx48g" java -jar target/*with-deps.jar \
   --bounds=planet \
   --max_point_buffer=4 \
   --exclude_layers=housenumber \
-  --download-threads=8 \
+  --download-threads=6 \
   --download-chunk-size-mb=1000 \
-  --threads=8 \
-  --nodemap-type=array --storage=ram 2>&1 | tee logs.txt
+  --threads=6 \
+  --free-osm-after-read \
+  --nodemap-type=array --storage=mmap 2>&1 | tee logs.txt
