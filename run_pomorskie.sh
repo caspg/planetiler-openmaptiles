855c1c0fb9e3b2062a7e0abaeb5bb4b80f53d97c
Kacper Golinski <kacper.golinski@gmail.com>
2024-04-26 10:26:07 +0200

    Update run_pomorskie.sh

--- run_pomorskie.sh
+++ run_pomorskie.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-JAVA_TOOL_OPTIONS="-Xmx10g" java -jar target/*with-deps.jar \
+JAVA_TOOL_OPTIONS="-Xmx4g" java -jar target/*with-deps.jar \
   --force \
   --download \
   --area=pomorskie  \
@@ -11,4 +11,4 @@ JAVA_TOOL_OPTIONS="-Xmx10g" java -jar target/*with-deps.jar \
   --download-threads=5 --download-chunk-size-mb=1000 \
   --threads=5 \
   —gzip-temp \
-  --nodemap-type=array --storage=ram 2>&1 | tee logs.txt
+  --nodemap-type=array --storage=mmap 2>&1 | tee logs.txt
